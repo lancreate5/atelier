b070b3d67e2e3989aa9f20dfdbfd28a644bc831b
Allen <valternatean13@gmail.com>
2023-04-26 16:17:08 +0700

    add: interactivity on main navbar

--- story/char-files/YurikaStrava/main.js
+++ story/char-files/YurikaStrava/main.js
@@ -1,1 +1,19 @@
-console.log("Ready!");
+// Change color on hover
+topNavMenus = document.querySelector("nav").querySelector("ul").childNodes;
+for (let ptr = 0; ptr < topNavMenus.length; ptr++) {
+	if(topNavMenus[ptr].nodeName == "LI") {
+		let menu = topNavMenus[ptr].querySelector("a");
+		
+		if(menu != null) {
+			// Shows underline on hover
+			menu.addEventListener("mouseover", () => {
+				menu.style.textDecoration = "underline";
+			});
+		
+			// Removes underline when unhovered
+			menu.addEventListener("mouseout", () => {
+				menu.style.textDecoration = "none";
+			});
+		}
+	}
+}
