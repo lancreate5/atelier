03382de4432ac3726ad24670507cb6ccdcf260ac
Allen <valternatean13@gmail.com>
2023-04-26 19:16:04 +0700

    feat: secondary nav highlight

--- story/char-files/YurikaStrava/main.js
+++ story/char-files/YurikaStrava/main.js
@@ -1,5 +1,5 @@
 // Change color on hover
-topNavMenus = document.querySelector("nav").querySelector("ul").childNodes;
+let topNavMenus = document.getElementsByClassName("right-nav")[0].childNodes;
 for (let ptr = 0; ptr < topNavMenus.length; ptr++) {
 	if(topNavMenus[ptr].nodeName == "LI") {
 		let menu = topNavMenus[ptr].querySelector("a");
@@ -17,3 +17,37 @@ for (let ptr = 0; ptr < topNavMenus.length; ptr++) {
 		}
 	}
 }
+
+let secondNavMenus = document.getElementsByClassName("content-nav")[0];
+let secondNavMenusName = [];
+let selected = secondNavMenus.getElementsByClassName("default")[0].textContent;
+
+for (let ptr = 0; ptr < secondNavMenus.childNodes.length; ptr++) {
+	let menu = secondNavMenus.childNodes[ptr];
+	if(menu.nodeName == "LI") {
+		secondNavMenusName[menu.textContent] = menu;
+
+		if(menu != null) {
+			menu.addEventListener("click", () => {
+				secondNavMenusName[selected].style.background = "#EEEEEE"
+				selected = menu.textContent;
+				secondNavMenusName[selected].style.background = "#FFFFFF"
+			});
+
+			// Shows underline on hover
+			menu.addEventListener("mouseover", () => {
+				if(menu.textContent != selected) {
+					menu.style.background = "#DDDDFF"
+				}
+			});
+		
+			// Removes underline when unhovered
+			menu.addEventListener("mouseout", () => {
+				if(menu.textContent != selected) {
+					menu.style.background = "#EEEEEE"
+				}
+			});
+		}
+	}
+}
+
